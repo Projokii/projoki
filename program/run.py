aa936855d5f1d76e73790cc2b775b27720db703c
Projokii <141165642+Projokii@users.noreply.github.com>
2024-11-19 18:01:41 +0700

    Update run.py

--- program/run.py
+++ program/run.py
@@ -1,19 +1,18 @@
 import asyncio
 import os
-from modul.proxy_handler import main  # Import fungsi utama
+from modul.proxy_handler import main
 
 async def main_script():
-    # Gunakan path absolut untuk file userid.txt
     user_id_path = os.path.abspath('../data/userid.txt')
-    if not os.path.exists(user_id_path):
+    try:
+        with open(user_id_path, 'r') as f:
+            user_id = f.read().strip()
+    except FileNotFoundError:
         print(f"File {user_id_path} tidak ditemukan.")
         return
 
-    with open(user_id_path, 'r') as f:
-        user_id = f.read().strip()  # Membaca user_id dari file userid.txt
-
-    # Menjalankan fungsi main dengan user_id
-    await main('bahan/proxy_1.txt', user_id)  # Pastikan path proxy benar
+    proxy_file_path = os.path.abspath('../bahan/proxy_1.txt')
+    await main(proxy_file_path, user_id)
 
 if __name__ == "__main__":
     asyncio.run(main_script())
