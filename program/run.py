de7a1b318657426b24a4edce3e22fdac553434ea
Projokii <141165642+Projokii@users.noreply.github.com>
2024-11-19 11:40:48 +0700

    Update run.py

--- program/run.py
+++ program/run.py
@@ -2,20 +2,24 @@ import asyncio
 import sys
 import os
 
-# Menambahkan path modul ke sys.path
+# Menambahkan path modul ke sys.path agar Python dapat menemukan file proxy_handler.py
 sys.path.append(os.path.join(os.path.dirname(__file__), '../modul'))
 
-from proxy_handler import main  # Sekarang Python dapat menemukan proxy_handler.py
+# Mengimpor fungsi main dari proxy_handler
+from proxy_handler import main
 
 async def main_script():
     try:
-        with open('userid.txt', 'r') as f:
-            user_id = f.read().strip()  # Membaca user_id dari file
+        # Membaca user_id dari file userid.txt yang ada di folder 'data'
+        with open('../data/userid.txt', 'r') as f:
+            user_id = f.read().strip()  # Menghapus spasi atau karakter tak terlihat
     except FileNotFoundError:
         print("File 'userid.txt' tidak ditemukan.")
         return
 
-    await main('proxy_1.txt', user_id)  # Menggunakan proxy_1.txt untuk skrip 1.py
+    # Menjalankan fungsi main dari proxy_handler dan memberikan proxy_1.txt serta user_id sebagai parameter
+    await main('../bahan/proxy_1.txt', user_id)
 
+# Menjalankan skrip utama
 if __name__ == "__main__":
     asyncio.run(main_script())
