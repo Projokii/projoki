3b9f7abbfa907c02f9c12ea428fe741b3899f851
Projokii <141165642+Projokii@users.noreply.github.com>
2024-11-19 13:40:03 +0700

    Update run.py

--- program/run.py
+++ program/run.py
@@ -10,10 +10,13 @@ from proxy_handler import main
 
 async def main_script():
     try:
-        with open('userid.txt', 'r') as f:
+        # Memastikan path yang benar ke 'userid.txt' di folder 'data'
+        userid_file_path = os.path.join(os.path.dirname(__file__), '../data', 'userid.txt')
+        
+        with open(userid_file_path, 'r') as f:
             user_id = f.read().strip()  # Membaca user_id dari file
     except FileNotFoundError:
-        print("File 'userid.txt' tidak ditemukan.")
+        print(f"File '{userid_file_path}' tidak ditemukan.")
         return
 
     await main('proxy_1.txt', user_id)  # Menggunakan proxy_1.txt untuk skrip 1.py
